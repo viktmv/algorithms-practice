6fc542b65335612ee6e8151a45aa32abb265ad33
Victor T <viktmv@icloud.com>
2017-07-12 20:27:40 -0400

    Refactor to improve output readability

--- tests/uniqueChars-test.js
+++ tests/uniqueChars-test.js
@@ -1,32 +1,34 @@
 const {expect} = require('chai')
 const {hasUniqueChars, hasUniqueCharsBrute, hasUniqueCharsSimple} = require('../chap1/uniqueChars')
 
-describe('hasUniqueChars', () => {
-  it('returns true if all characters in string are unique', () => {
-    expect(hasUniqueChars('cat')).to.equal(true)
+describe('uniqueChars', () => {
+  describe('hasUniqueChars', () => {
+    it('returns true if all characters in string are unique', () => {
+      expect(hasUniqueChars('cat')).to.equal(true)
+    })
+
+    it('returns false if characters in string are not unique', () => {
+      expect(hasUniqueChars('hello')).to.equal(false)
+    })
   })
 
-  it('returns false if characters in string are not unique', () => {
-    expect(hasUniqueChars('hello')).to.equal(false)
-  })
-})
+  describe('hasUniqueCharsSimple', () => {
+    it('returns true if all characters in string are unique', () => {
+      expect(hasUniqueCharsSimple('cat')).to.equal(true)
+    })
 
-describe('hasUniqueCharsSimple', () => {
-  it('returns true if all characters in string are unique', () => {
-    expect(hasUniqueCharsSimple('cat')).to.equal(true)
-  })
-
-  it('returns false if characters in string are not unique', () => {
-    expect(hasUniqueCharsSimple('hello')).to.equal(false)
-  })
-})
-
-describe('hasUniqueCharsBrute', () => {
-  it('returns true if all characters in string are unique', () => {
-    expect(hasUniqueCharsBrute('dragon')).to.equal(true)
+    it('returns false if characters in string are not unique', () => {
+      expect(hasUniqueCharsSimple('hello')).to.equal(false)
+    })
   })
 
-  it('returns false if characters in string are not unique', () => {
-    expect(hasUniqueCharsBrute('giraffe')).to.equal(false)
+  describe('hasUniqueCharsBrute', () => {
+    it('returns true if all characters in string are unique', () => {
+      expect(hasUniqueCharsBrute('dragon')).to.equal(true)
+    })
+
+    it('returns false if characters in string are not unique', () => {
+      expect(hasUniqueCharsBrute('giraffe')).to.equal(false)
+    })
   })
 })
