947d1883a05836781390f956a3314c90472aed61
Victor T <viktmv@icloud.com>
2017-07-14 18:17:47 -0400

    Add basic implementation of compress string function

---
+++ chap1/compressString.js
@@ -0,0 +1,21 @@
+// Perform basic string compression with letter count
+function compress(str) {
+  let counter = {}
+  let compressed = ''
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]
+    let next = str[i + 1]
+
+    if (!counter[char]) counter[char] = 1
+    else counter[char]++
+
+    if (char !== next) {
+      compressed += (char + counter[char])
+      counter[char] = 0
+    }
+  }
+  return compressed.length >= str.length ? str : compressed
+}
+
+console.log(compress('aabcccccaaa'))
